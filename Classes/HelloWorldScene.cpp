cb99343b6b0da35969a7904147556bcca83c12e7
fujisawa <sfujisawa2016@gmail.com>
2019-01-19 15:43:38 +0900

    やってみよう２まで

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -148,6 +148,19 @@ bool HelloWorld::onTouchBegan(Touch* touch, Event* unused_event)
 		{
 			// タッチした時に実行したい処理
 			log("touch sprite!!");
+
+			// エフェクトを発生
+			Sprite* effect = Sprite::create("effect3.png");
+			effect->setPosition(spr->getPosition());
+			this->addChild(effect);
+			// エフェクトにアクションをつける
+			ScaleTo* scale = ScaleTo::create(0.5f, 2.0f);
+			FadeOut* fade = FadeOut::create(0.5f);
+			RemoveSelf* remove = RemoveSelf::create();
+			Sequence* fadeseq = Sequence::create(fade, remove, nullptr);
+			effect->runAction(scale);
+			effect->runAction(fadeseq);
+
 			// スプライトを削除
 			spr->removeFromParent();
 			spr = nullptr; // スプライトは空です
