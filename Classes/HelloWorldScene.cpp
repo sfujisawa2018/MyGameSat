4f045e39a92960ebc4310b5c5b8194d9d2de658e
fujisawa <sfujisawa2016@gmail.com>
2019-01-26 14:31:08 +0900

    下に落ちるところまで

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -167,18 +167,21 @@ bool HelloWorld::onTouchBegan(Touch* touch, Event* unused_event)
 				spr[i]->stopAllActions();
 				//// タッチした時に実行したい処理
 				//log("touch sprite!!");
+				// 下に落ちるアクション
+				MoveBy* moveBy = MoveBy::create(1.0f, Vec2(0, -720));
+				spr[i]->runAction(moveBy);
 
-				//// エフェクトを発生
-				//Sprite* effect = Sprite::create("effect3.png");
-				//effect->setPosition(spr->getPosition());
-				//this->addChild(effect);
-				//// エフェクトにアクションをつける
-				//ScaleTo* scale = ScaleTo::create(0.5f, 2.0f);
-				//FadeOut* fade = FadeOut::create(0.5f);
-				//RemoveSelf* remove = RemoveSelf::create();
-				//Sequence* fadeseq = Sequence::create(fade, remove, nullptr);
-				//effect->runAction(scale);
-				//effect->runAction(fadeseq);
+				// エフェクトを発生
+				Sprite* effect = Sprite::create("effect3.png");
+				effect->setPosition(spr[i]->getPosition());
+				this->addChild(effect);
+				// エフェクトにアクションをつける
+				ScaleTo* scale = ScaleTo::create(0.5f, 2.0f);
+				FadeOut* fade = FadeOut::create(0.5f);
+				RemoveSelf* remove = RemoveSelf::create();
+				Sequence* fadeseq = Sequence::create(fade, remove, nullptr);
+				effect->runAction(scale);
+				effect->runAction(fadeseq);
 
 				//// スプライトを削除
 				//spr->removeFromParent();
